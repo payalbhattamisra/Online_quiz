a24c82188a9a99b2efb42bb6ce48a31bfc32dcb8
govinda-mahanti <gmahanti955@gmail.com>
2024-05-06 22:42:33 +0530

    change

--- quiz/src/Components/Footer/Footer.jsx
+++ quiz/src/Components/Footer/Footer.jsx
@@ -3,7 +3,37 @@ import React from 'react'
 function Footer() {
   return (
     <>
-    
+      <div className="footer">
+        <div>
+          <div>
+            <h1>ज्ञानकोश</h1>
+          </div>
+          <div>
+          <div className='footerDetails'>
+            <div><a href="/">REGISTER</a></div>
+            <div><a href="/">CONTACT</a></div>
+            <div><a href="/">OUR TEAM</a></div>
+          </div>
+          </div>
+          <div className='footerSocial'>
+            <p>SOCIALIZE WITH US</p>
+            <div className='sociaMedia'>
+              <div><a href="/"><i className="fa-brands fa-instagram"></i></a></div>
+              <div><a href="/"><i className="fa-brands fa-twitter "></i></a></div>
+              <div><a href="/"><i className="fa-brands fa-facebook-f"></i></a></div>
+              <div><a href="/"><i className="fa-brands fa-linkedin-in"></i></a></div>
+            </div>
+          </div>
+        </div>
+        <div className="dev">
+            <div><a href="/"></a></div>
+            <div><a href="/"><i className="fa-brands fa-twitter "></i></a></div>
+
+        </div>
+        <div className="copyR">
+          Copyright @2024 Gyankosh, All Rights Reserved 
+        </div>
+      </div>
     </>
   )
 }
