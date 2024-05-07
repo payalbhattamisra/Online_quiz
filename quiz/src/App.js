84018d67b2e32f00369300e34b8f861beb7bf970
Payal <payalbhattamisra@gmail.com>
2024-05-07 21:50:12 +0530

    changes

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import './Components/Navbar/Navbar';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
+import Banner from './Components/Banner/Banner';
 function App() {
   return (
     <> 
     <Navbar/>
+    <Banner/>
     <Footer/>
-    </>
+     </>
   );
 }
 
---
+++ quiz/src/Components/Banner/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Banner.css";
+function Banner() {
+  return (
+    <>
+      <div className='page1'>
+        <div className="part1"> 
+        <h1>Free Online <span>quiz maker</span></h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Banner
