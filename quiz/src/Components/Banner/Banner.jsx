6aa4f6b6437c4fb7407d40c3c0c6e57829fa3491
Payal <payalbhattamisra@gmail.com>
2024-05-09 00:51:46 +0530

    bann

--- quiz/src/Components/Banner/Banner.jsx
+++ quiz/src/Components/Banner/Banner.jsx
@@ -6,8 +6,11 @@ function Banner() {
       <div className='page1'>
         <div className="part1"> 
         <h1>Free Online  quiz maker</h1>
+        </div>
         <div className='page2'>
-          <div></div>
+          <p>Make a quiz with different question types to engage students in a classroom,
+          <br/><p>  train employees at work, or play trivia with friends.</p></p>  
+          <div> 
         </div>
         </div>
       </div>
