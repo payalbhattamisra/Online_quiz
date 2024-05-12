51ae5042e9f8e81d7966c57afeb8dbf8a364044d
Payal <payalbhattamisra@gmail.com>
2024-05-12 16:31:23 +0530

    img

--- quiz/src/Components/Types/Types.jsx
+++ quiz/src/Components/Types/Types.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
+import React from 'react';
 import './Types.css';
+
 function Types() {
+  const showImage = (imageUrl) => {
+    const displayedImage = document.getElementById('displayedImage');
+    displayedImage.src = imageUrl;
+    const imageContainer = document.getElementById('imageContainer');
+    imageContainer.style.display = 'block';
+  };
+
   return (
     <div className='Types'>
       <h1>So Many Question Types to Choose From</h1>
       <div className="btns">
-       
-        <button class="button button1"> Fill in the blank</button>
-        <button class="button button2">Reorder</button>
-        <button class="button button3">Multiple choice</button>
-        <button class="button button4">match</button>
-        <button class="button button5">Draw</button>
-
+        <button className="button1" onClick={() => showImage('./Components/pics/s1.png')}>Fill in the blank</button>
+        <button className="button1" onClick={() => showImage('./Components/pics/s2.png')}>Reorder</button>
+        <button className="button1" onClick={() => showImage('./Components/pics/s3.png')}>Multiple choice</button>
+        <button className="button1" onClick={() => showImage('./Components/pics/s4.png')}>Match</button>
+        <button className="button1" onClick={() => showImage('./Components/pics/s5.png')}>Draw</button>
+        <button className="button1" onClick={() => showImage('./Components/pics/s6.png')}>Drag & Drop</button>
+      </div>
+      <div className='btnsss'>
+        <button className="button1" onClick={() => showImage('./Components/pics/s7.png')}>Video Response</button>
+        <button className="button1" onClick={() => showImage('./Components/pics/s8.png')}>Poll</button>
+        <button className="button1" onClick={() => showImage('./Components/pics/s9.png')}>Slides</button>
+        <button className="button1" onClick={() => showImage('./Components/pics/s10.png')}>Open ended</button>
+        <button className="button1" onClick={() => showImage('./Components/pics/s11.png')}>Drop down</button>
+      </div>
+      <div id="imageContainer" style={{ display: 'none' }}>
+        <img src="./Components/pics/s1.png" alt="Displayed Image" id="displayedImage" />
       </div>
     </div>
-  )
+  );
 }
 
-export default Types
+export default Types;
