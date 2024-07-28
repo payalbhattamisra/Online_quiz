ec07adebbf7021fd45e0b7d8707584a296a3106a
govinda-mahanti <gmahanti955@gmail.com>
2024-07-28 11:57:00 +0530

    new dependencies cloudinary and multer,

---
+++ quiz/Backend/src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import cloudinary from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadCloudinary = async (localFilePath)=>{
+    try {
+        if( !localFilePath) return null;
+        const response= await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+
+        fs.unlinkSync(localFilePath)
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null;
+    }
+}
+
+export default uploadCloudinary
