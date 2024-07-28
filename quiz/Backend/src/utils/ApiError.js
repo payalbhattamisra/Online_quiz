232a26873c27bc332691332a01050a14ee5d91ab
govinda-mahanti <gmahanti955@gmail.com>
2024-07-28 10:49:31 +0530

    add utilities

---
+++ quiz/Backend/src/utils/ApiError.js
@@ -0,0 +1,18 @@
+class ApiError extends Error{
+    constructor(statusCode, message="somthing went wrong", errors=[], stack=""){
+        super(message);
+        this.statusCode=statusCode;
+        this.message=message;
+        this.errors=errors;
+        this.data=null;
+        this.success=false;
+
+
+        if(stact){
+            this.stack=stack;
+        }
+        else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
